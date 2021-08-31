3d03d421cd55ec0a06fef086be3d47b19b3c80ea
carlosdizx <carlodiaz@umariana.edu.co>
2021-08-30 21:15:04 -0500

    Funcionando el metodo de crear

--- mascota.php
+++ mascota.php
@@ -1,1 +1,16 @@
 <?php
+include_once("index.php");
+
+// Consulta todos los registros de la tabla mascotas de un usuario dado su id
+if (isset($_GET["all_propietario"])){
+    $id = $_GET["all_propietario"];
+    $query = "SELECT * FROM mascotas WHERE dueño = '$id'";
+    $sql = mysqli_query($conexionBD,$query );
+    if (!$sql){
+        echo json_encode([["mensaje"=>"No hay datos"]]);
+    }
+    else{
+        $mascotas = mysqli_fetch_all($sql,MYSQLI_ASSOC);
+        echo json_encode($mascotas);
+    }
+}
